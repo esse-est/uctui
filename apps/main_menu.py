6fca651b688211f6c0c85006b0cc11e0e760cbbe
alittles1lly <ijonkventar@gmail.com>
2025-03-03 02:21:36 -0600

    linted

--- apps/main_menu.py
+++ apps/main_menu.py
@@ -2,102 +2,109 @@ from unicurses import *
 
 io_command = ""
 
+
 def x_at_half(string=""):
-    if len(string) != 0: #so empty string doesnt crash
-        location = (getmaxx(window)//2)-len(string)//2
+    if len(string) != 0:  # so empty string doesnt crash
+        location = (getmaxx(window) // 2) - len(string) // 2
     else:
-        location = (getmaxx(window)//2)
-    return(location)
+        location = getmaxx(window) // 2
+    return location
+
 
 class main:
-    
-    
+
     def declare_req(box: str, source_config: dict, source_logger):
         global window, logger, config
         window = box
         logger = source_logger
         config = source_config
-    
+
     def load():
-        
-        y_max,x_max = getmaxyx(window)
+
+        y_max, x_max = getmaxyx(window)
         clear()
-        mvaddstr(0,x_at_half("main_menu"),"main_menu",A_BOLD)
-        
-        #building window boxes, note box()/border() not used as its 1 scr
-        setsyx(1,x_at_half())
-        vline("|",y_max-2)
-        setsyx(1,0)
-        hline("-",x_max)
-        
-        #bad math for 1 liners T-T
-        mvaddstr(2,(x_max//4-(len("Loaded Apps:")//2)),"Loaded Apps:",A_BOLD)
+        mvaddstr(0, x_at_half("main_menu"), "main_menu", A_BOLD)
+
+        # building window boxes, note box()/border() not used as its 1 scr
+        setsyx(1, x_at_half())
+        vline("|", y_max - 2)
+        setsyx(1, 0)
+        hline("-", x_max)
+
+        # bad math for 1 liners T-T
+        mvaddstr(2, (x_max // 4 - (len("Loaded Apps:") // 2)), "Loaded Apps:", A_BOLD)
         app_counter = 0
         for i in config["enabled_apps"]:
             if app_counter < y_max:
-                app_counter+=1
-                mvaddstr((app_counter+2),(x_max//4-(len(i)//2)),i)
-        
-        
-        mvaddstr(2,(round(x_max*0.75)-len("Commands:")//2),"Commands:",A_BOLD)
-        
-        
-        setsyx(getmaxy(window)-1,0)
-        addstr("to_run:",A_BOLD)
-        
-        logger.debug("main_menu module init")
+                app_counter += 1
+                mvaddstr((app_counter + 2), (x_max // 4 - (len(i) // 2)), i)
 
+        mvaddstr(2, (round(x_max * 0.75) - len("Commands:") // 2), "Commands:", A_BOLD)
+        cmd_counter = 0
+        for i in commands.cmd_list:
+            if cmd_counter < y_max:
+                cmd_counter += 1
+                mvaddstr((cmd_counter + 2), (round(x_max * 0.75) - len(i) // 2), i)
+
+        setsyx(getmaxy(window) - 1, 0)
+        addstr("to_run:", A_BOLD)
+
+        logger.debug("main_menu module init")
 
     def loop():
         global io_command
-        
+
         while True:
             y, x = getsyx()
             io_stream = getkey()
-            
-            io_command=io_command.strip()
-            
+
+            io_command = io_command.strip()
+
             if len(io_stream) == 1:
                 addch(io_stream)
                 io_command += io_stream
-            
+
             elif io_stream == "KEY_BACKSPACE" and getsyx()[1] > 7:
                 io_command = io_command[:-1]
-                mvaddstr(y,x-1," ")
-                setsyx(y,x-1)
-            
-            elif io_stream == "^J" and len(io_stream) != 0: #running a command
-                
-                if io_command.startswith("load"): #extra if for load() as y'need the name
+                mvaddstr(y, x - 1, " ")
+                setsyx(y, x - 1)
+
+            elif io_stream == "^J" and len(io_stream) != 0:  # running a command
+
+                if io_command.startswith(
+                    "load"
+                ):  # extra if for load() as y'need the name
                     load_split = io_command.split("*")
                     if len(load_split) == 3 and len(load_split[1]) != 0:
                         commands.load(io_command.split("*")[1])
-                
-                #has to be here as return() needs to be in loop()
-                elif io_command == "quit": 
-                    return("NULL")
-                
+
+                # has to be here as return() needs to be in loop()
+                elif io_command == "quit":
+                    return "NULL"
+
                 elif io_command.lower() in commands.user_alias:
                     commands.user_alias[io_command]()
+
                 else:
                     logger.warning("user tried to run erroring command")
-                
-                mvaddstr(y,7,(" "*len(io_command)))
-                
-                setsyx(y,7)
+
+                mvaddstr(y, 7, (" " * len(io_command)))
+
+                setsyx(y, 7)
+
 
 class commands:
     global command_list
-    
+
     def load(app: str):
         if os.path.isfile(f"apps/{app}.py"):
-            return(app)
+            return app
         else:
             logger.warning("user tried to load non-existent app")
-    
+
     def config():
         pass
 
-    
-    user_alias = {"load": load
-                    , "cfg": config} #user typed strs for ran commands
+    cmd_list = ["load", "cfg", "quit"]
+
+    user_alias = {"load": load, "cfg": config}  # user typed strs for ran commands
--- uctui.py
+++ uctui.py
@@ -9,49 +9,52 @@ logger = logging.getLogger(__name__)
 config = {}
 stdscr = initscr()
 io_state = ""
-loaded = {} #dict of loaded modules
+loaded = {}  # dict of loaded modules
+
 
 def init():
     global io_state
-    
-    get_time=time.strftime("%Y-%m-%d", time.localtime())
-    
-    
+
+    get_time = time.strftime("%Y-%m-%d", time.localtime())
+
     if config["do_logs"] == "True":
         fh = logging.FileHandler(filename=f"logs/{get_time}.log")
         logging.basicConfig(
             format="%(asctime)s,%(msecs)d %(levelname)s %(message)s",
             level=logging.INFO,
-            handlers=[fh]
+            handlers=[fh],
         )
     noecho()
     cbreak()
-    keypad(stdscr,True)
+    keypad(stdscr, True)
     if config["is_color"] == "True":
         start_color()
     curs_set(2)
     io_state = "main_menu"
 
+
 def close():
     echo()
     nocbreak()
-    keypad(stdscr,False)
+    keypad(stdscr, False)
     endwin()
 
-def pull_config(): #pulls config.txt, strips it, removes comments, and formats it into config: dict
+
+def pull_config():  # pulls config.txt, strips it, removes comments, and formats it into config: dict
     with open("config.txt", "r") as cf:
         for i in cf.readlines():
-            i=i.replace("\n","").replace(" ", "")
+            i = i.replace("\n", "").replace(" ", "")
             if not i.startswith("#") and len(i) != 0:
-                i=i.split(":")
+                i = i.split(":")
                 if len(i) == 2:
                     config[i[0]] = i[1]
-    
+
     if len(config["enabled_apps"]) > 2:
         for i in config["enabled_apps"].replace("[", "").replace("]", "").split(","):
             if type(config["enabled_apps"]) != list:
                 config["enabled_apps"] = []
             config["enabled_apps"].append(i)
+
 
 def pull_apps():
     enabled_holder = []
@@ -59,31 +62,33 @@ def pull_apps():
         if os.path.isfile(f"apps/{app}.py"):
             loaded[app] = importlib.import_module(f"apps.{app}")
             enabled_holder.append(app)
-    
-    config["enabled_apps"]=enabled_holder #update to remove non-existent apps
+
+    config["enabled_apps"] = enabled_holder  # update to remove non-existent apps
+
 
 def main():
     return_direction = "main_menu"
     while True:
         if return_direction in config["enabled_apps"]:
-            loaded[return_direction].main.declare_req(stdscr,config,logger)
+            loaded[return_direction].main.declare_req(stdscr, config, logger)
             loaded[return_direction].main.load()
-            
-            #allows script to return to here before going elsewhere
-            return_direction=loaded[return_direction].main.loop()
-        
+
+            # allows script to return to here before going elsewhere
+            return_direction = loaded[return_direction].main.loop()
+
         elif return_direction == "NULL":
             break
-        
+
         else:
             logger.critical("tried loading app not found")
             break
 
+
 pull_config()
 pull_apps()
 
-init() #unicurses stuff
+init()  # unicurses stuff
 
-main() #effectively just "main_menu wrapper"
+main()  # effectively just "main_menu wrapper"
 
-close() #closes unicurses out
+close()  # closes unicurses out
